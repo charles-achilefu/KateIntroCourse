992e570253b4fb4e09f1dd1a92319f8266c2ecdd
SergeevAlexander <arzhanok@mail.ru>
2017-02-03 21:36:13 +0300

    Functions and Headers

--- KateIntroCourse/KateIntroCourse.cpp
+++ KateIntroCourse/KateIntroCourse.cpp
@@ -6,28 +6,11 @@
  
 using namespace std;
 
+// Functions and Headers
+
 int main()
 {
-	int GuessNumber, Answer = 7;
-	bool bShouldContinueGame = true;
-	while (bShouldContinueGame)
-	{
-		cout << "Enter a number : ";
-		cin >> GuessNumber;
-		if (GuessNumber == Answer)
-		{
-			cout << endl << "Got it!" << endl;
-			bShouldContinueGame = false;
-		} 
-		else if(GuessNumber < Answer)
-		{
-			cout << endl << "GuessNumber < Answer" << endl;
-		}
-		else
-		{
-			cout << endl << "GuessNumber > Answer" << endl;
-		}
-	}
+	
 
     return 0;
 }
