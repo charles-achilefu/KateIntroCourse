3581259b8967c132cd1000d8daabd1f2998d37b4
SergeevAlexander <arzhanok@mail.ru>
2017-02-03 19:01:53 +0300

    Guess Number

--- KateIntroCourse/KateIntroCourse.cpp
+++ KateIntroCourse/KateIntroCourse.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+ 
+using namespace std;
 
 int main()
 {
+	int GuessNumber, Answer = 7;
+	bool bShouldContinueGame = true;
+	while (bShouldContinueGame)
+	{
+		cout << "Enter a number : ";
+		cin >> GuessNumber;
+		if (GuessNumber == Answer)
+		{
+			cout << endl << "Got it!" << endl;
+			bShouldContinueGame = false;
+		} 
+		else if(GuessNumber < Answer)
+		{
+			cout << endl << "GuessNumber < Answer" << endl;
+		}
+		else
+		{
+			cout << endl << "GuessNumber > Answer" << endl;
+		}
+	}
+
     return 0;
 }
 
